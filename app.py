90ab4677b9eebea2f4119370764e9203133ad8de
Priyanshu Verma <priyanshuv826@gmail.com>
2024-10-22 01:21:40 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+# create a dictinory
+student = {
+    'Pawan' : 100,
+    'Rohan' : 200,
+}
+print(student)
+
+# Accessing a element
+# print(student['Pawan'])
+
+# #update 
+# student['Pawan'] = 300
+# print(student)
+
+# delete 
+del student['Pawan']
+print(student)
